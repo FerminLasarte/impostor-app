afddab8423ad644e19fb77cc5dd01d0d58bdf0d5
FerminLasarte <fermin.lasarte@icloud.com>
2025-12-14 13:28:38 -0300

    firebase conectado

--- impostorapp/App/impostorappApp.swift
+++ impostorapp/App/impostorappApp.swift
@@ -1,10 +1,23 @@
 import SwiftUI
+import FirebaseCore // 1. Importar el módulo base
+
+// 2. Crear el adaptador para el AppDelegate
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure() // 3. ¡La línea mágica!
+    return true
+  }
+}
 
 @main
 struct impostorappApp: App {
+    // 4. Inyectar el delegado
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            HomeView() // Asumiendo que ya creaste la HomeView del paso anterior
         }
     }
 }
